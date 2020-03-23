37a57d204dfc05c783ee12f639493ba433ca3274
Soudini <arnaud.louys@student-cs.fr>
2020-03-23 17:00:39 +0100

    added pathfinding feateures
    Former-commit-id: 35a07285201b66f4680506500fecbc3483e8b9f2

--- distance_correlation.py
+++ distance_correlation.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from import_data import import_data
 
+
 def correlation(distance_field):
     stations = import_data()
     initial_detection_times = np.array([stations[s].initial_time for s in stations])
@@ -9,4 +10,4 @@ def correlation(distance_field):
     normalized_initial_detection_times = initial_detection_times / np.linalg.norm(initial_detection_times)
     normalized_distance_field = distance_field / np.linalg.norm(distance_field, axis = 2, keepdims=True)
 
-    return np.tensordot(normalized_distance_field, normalized_initial_detection_times, axis = [2,0])
+    return np.tensordot(normalized_distance_field, normalized_initial_detection_times, axes = [2,0])
