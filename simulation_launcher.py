5b4c9f9c3873860cf997a654d663c534f0aee8b1
Soudini <arnaud.louys@student-cs.fr>
2020-04-02 14:56:16 +0200

    do not relaunch the simulation if it has already been done
    Former-commit-id: 68efe0c85f955746d56f695924f1d626e9eb38ba

--- simulation_launcher.py
+++ simulation_launcher.py
@@ -6,8 +6,12 @@ import time
 
 def launch_simulation(XC = 40, YC = 65, M_TNT = 10, PAS = 1):
     folder_name = "CAS_SB_2D_" + str(float(PAS)*1e2) + "_cm_" + str(float(XC)) + "_" + str(float(YC)) + "_" + str(float(M_TNT)) + "kg"
-    # subprocess.Popen(['python', './SouffleBati2D.py', str(XC), str(YC), str(M_TNT), str(PAS)], stdin=subprocess.PIPE, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, close_fds=True )
-    subprocess.Popen(['python', './fake_sim.py', folder_name], stdin=subprocess.PIPE, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, close_fds=True )
+    if os.path.exists(os.getcwd() + '/' + folder_name):
+        print('simulation already done, skipping it')
+        return folder_name
+    else:
+        # subprocess.Popen(['python', './SouffleBati2D.py', str(XC), str(YC), str(M_TNT), str(PAS)], stdin=subprocess.PIPE, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, close_fds=True )
+        subprocess.Popen(['python', './fake_sim.py', folder_name], stdin=subprocess.PIPE, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, close_fds=True )
     return folder_name
 
 def batch_launch_simulation(sim_list):
